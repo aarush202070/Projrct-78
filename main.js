e2e9125f47cf4c613084c98fa3c7129815790373
aarush202070 <82890981+aarush202070@users.noreply.github.com>
2021-06-22 21:13:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 var reasons = [
     "Dad",
-    "son",
+    "Son",
+    "Mom",
     
   ];
   
   var images = [
+    
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_CuXMC-TRnSoC2aIsU26F9x-EewCqWCEVLQ&usqp=CAU",
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmmn4nTdqpR1d0N1eoI_j0FOJotFuW2hQtNg&usqp=CAU",
+    "download.jpg",
   ];
   
   var i = 0;
-  function nextslide() {  document.getElementById("name").innerHTML = reasons[i];
+   
+  function nextslide()
+   
+    {  document.getElementById("name").innerHTML = reasons[i];
     document.getElementById("pic").src = images[i];
     i++;
 
-    
+    if(i == 3)
+    { i=0; }
 }
   
